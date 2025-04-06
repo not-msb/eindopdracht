a7b7b9acd7fa9a6592efcd74830ebba8965c48f8
not-msb <msembulut@gmail.com>
2025-04-06 17:31:34 +0200

    Explained question 14&15

--- vraag14.js
+++ vraag14.js
@@ -6,13 +6,36 @@ console.log('Tel vrienden ook mee als personen.');
 console.log('Bijvoorbeeld:\n...\nUzbekistan, aantal personen: 10\nGreece, aantal personen: 7\n...');
 
 // schrijf jouw code hier
-const antw = [...people.reduce(
-    (c, p) => p.friends.reduce(
-        (c, f) => c.set(f.location.country, c.get(f.location.country) + 1),
-        c.set(p.location.country, c.get(p.location.country) + 1)
-    ),
-    people.reduce((c, p) => p.friends.reduce((c, f) => c.set(f.location.country, 0), c.set(p.location.country, 0)), new Map())
-)].reduce((c, m) => m[1] >= 7 ? [...c, {land: m[0], aantal: m[1]}] : c, []);
+
+/*
+ * Bij deze vraag gebruiken we voor het eerst een helper functie, voornamelijk zodat mijn code niet *te* afgrijselijk lang word.
+ * De update method wrapt het map.set(key, callback(map.get() ?? or)) patroon, in een simpele functie
+ *
+ * In de reduces iterates we over alle personen en dan hun vrienden
+ * Eigenlijk zijn het een hoop 'length-patronen' die we in een map stoppen
+ *
+ * In de tweede fase converten we de map naar een iterator, zodat we er lekker weer reduce op kunnen callen
+ * m[0] is hier de key, en m[1] de value (als integer)
+ * Daarna doen we de standaard ternary filtering
+*/
+Map.prototype.update = function(key, callback, or) {
+    const v = this.get(key) ?? or;
+    return this.set(key, callback(v));
+};
+
+const antw = people
+    .reduce(
+        (c, p) => p.friends.reduce(
+            (c, f) => c.update(f.location.country, (v) => v+1, 0),
+            c.update(p.location.country, (v) => v+1, 0)
+        ),
+        new Map()
+    )
+    .entries()
+    .reduce((c, m) =>
+        m[1] >= 7 ? [...c, {land: m[0], aantal: m[1]}] : c,
+        []
+    );
 
 // Print hier het antwoord op de vraag 
 console.log('\n<Antwoord>');
--- vraag15.js
+++ vraag15.js
@@ -7,22 +7,42 @@ console.log('Bijvoorbeeld:\n...\nUzbekistan, hobbies: {"Basketball":6,"Swimming"
 
 // schrijf jouw code hier
 
-// De enige vraag waarbij ik meerdere stappen nodig had
+/*
+ * Hetzelfde concept als bij 15, maar nu gebruiken we fase 1 van vraag 14 direct
+ * We hebben nu de increment van de 'length-patronen' vervangen met reduces op de hobbies
+ * Eigenlijk dus nog een laag in de value van de Map toegevoegt
+ *
+ * In de tweede fase converten we de map naar een iterator, zodat we er lekker weer reduce op kunnen callen
+ * m[0] is hier de key, en m[1] de value (als list)
+ * We gebruiken m[0] om het aantal wonende personen te vinden in een land
+ * Daarna doen we de standaard ternary filtering
+*/
+Map.prototype.update = function(key, callback, or) {
+    const v = this.get(key) ?? or;
+    return this.set(key, callback(v));
+};
+
 const woningen = people.reduce(
     (c, p) => p.friends.reduce(
-        (c, f) => c.set(f.location.country, c.get(f.location.country) + 1),
-        c.set(p.location.country, c.get(p.location.country) + 1)
+        (c, f) => c.update(f.location.country, (v) => v+1, 0),
+        c.update(p.location.country, (v) => v+1, 0)
     ),
-    people.reduce((c, p) => p.friends.reduce((c, f) => c.set(f.location.country, 0), c.set(p.location.country, 0)), new Map())
+    new Map()
 );
 
-const antw = [...people.reduce(
-    (c, p) => p.friends.reduce(
-        (c, f) => c.set(f.location.country, f.hobbies.reduce((c, h) => c.set(h, (c.get(h) ?? 0) + 1), c.get(f.location.country))),
-        c.set(p.location.country, p.hobbies.reduce((c, h) => c.set(h, (c.get(h) ?? 0) + 1), c.get(p.location.country)))
-    ),
-    people.reduce((c, p) => p.friends.reduce((c, f) => c.set(f.location.country, new Map()), c.set(p.location.country, new Map())), new Map())
-)].reduce((c, m) => woningen.get(m[0]) > 7 ? [...c, {land: m[0], aantal: woningen.get(m[0]), hobbies: m[1]}] : c, []);
+const antw = people
+    .reduce(
+        (c, p) => p.friends.reduce(
+            (c, f) => c.update(f.location.country, (m) => f.hobbies.reduce((c, h) => c.update(h, (v) => v+1, 0), m), new Map()),
+            c.update(p.location.country, (m) => p.hobbies.reduce((c, h) => c.update(h, (v) => v+1, 0), m), new Map())
+        ),
+        new Map()
+    )
+    .entries()
+    .reduce((c, m) =>
+        woningen.get(m[0]) > 7 ? [...c, {land: m[0], aantal: woningen.get(m[0]), hobbies: m[1]}] : c,
+        []
+    );
 
 // Print hier het antwoord op de vraag 
 console.log('\n<Antwoord>');
